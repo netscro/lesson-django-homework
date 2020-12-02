3bc81bde7c8107702bb182e471d3c2486ff82e9d
netscro <kovalev_evgeniy@list.ru>
2020-12-02 15:25:49 +0200

    docstring

--- home/views.py
+++ home/views.py
@@ -5,8 +5,18 @@ from django.http import HttpResponse
 
 
 def home(request):
+    """
+    This is page 'home/'
+    :param request: output 'Hello world!'
+    :return: Hello world
+    """
     return HttpResponse('<h1>Hello world!</h1>')
 
 
 def main_page(request):
+    """
+    This is maim page '/'
+    :param request: output 'Hello, this is a home page :)'
+    :return: Hello, this is a home page :)
+    """
     return HttpResponse('<h1>Hello, this is a home page :)</h1>')
