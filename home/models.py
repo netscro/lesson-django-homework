cf405b3ccc7ee0ef01306da8c03e181c91294a3a
netscro <kovalev_evgeniy@list.ru>
2020-12-05 22:14:56 +0200

    add stydents page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    """
+    Student data of all students in database
+    """
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=200)
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from home.models import Student
 
 
 def home(request):
@@ -20,3 +21,14 @@ def main_page(request):
     :return: Hello, this is a home page :)
     """
     return render(request, 'index.html')
+
+
+def students(request):
+    """
+    This page print information about all students in database
+    :param request: output 'students.html'
+    :return: information of all students
+    """
+    student_data = Student()
+
+    return render(request, 'students.html')
