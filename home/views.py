401ee769e789a99480f2e91e4c6c6490d71fe873
netscro <kovalev_evgeniy@list.ru>
2020-12-17 15:45:44 +0200

    fix flake8

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 # Create your views here.
 from django.urls import reverse
 from django.views import View
