641ecb23dd3dcf51fc31347af63632872a5eafff
netscro <kovalev_evgeniy@list.ru>
2021-01-14 01:46:04 +0200

    fixed task currency save

--- home/tasks.py
+++ home/tasks.py
@@ -12,13 +12,12 @@ def privatbank_currency():
 
 @shared_task
 def currency_model_save(currency):
-    currency_model = Currency()
     for new_currency in currency:
-        for save_currency in new_currency:
-            currency_model.ccy = save_currency['ccy']
-            currency_model.base_ccy = save_currency['base_ccy']
-            currency_model.buy = save_currency['buy']
-            currency_model.sale = save_currency['sale']
+        currency_model = Currency()
+        currency_model.ccy = new_currency['ccy']
+        currency_model.base_ccy = new_currency['base_ccy']
+        currency_model.buy = new_currency['buy']
+        currency_model.sale = new_currency['sale']
         currency_model.save()
 
 
