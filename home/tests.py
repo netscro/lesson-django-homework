cca7536d11efe6e489692607b1c035afd767c4e4
netscro <kovalev_evgeniy@list.ru>
2021-03-16 23:55:11 +0200

    add test create student to api view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,4 @@
+from unittest import skip
 
 from django.test import TestCase
 
@@ -5,22 +6,62 @@ from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from home.models import Student
+from home.models import Student, Teacher
 
 
 class StudentsApiTest(APITestCase):
 
+    @skip
     def test_students_list_view(self):
         response = self.client.get(reverse('students_api-list'))
-        empty_base_response = {
+        base_response = {
             'count': 0,
             'next': None,
             'previous': None,
             'results': []
         }
-        self.assertEqual(response.json(), empty_base_response)
+        self.assertEqual(response.json(), base_response)
 
+    @skip
+    def test_student_create(self):
+        Teacher.objects.create(name_surname='Ted')
+        response = self.client.post(reverse('students_api-list'),
+                                    {
+                                        'name': 'One',
+                                        'surname': 'student',
+                                        'teacher': [1],
+                                    })
+        base_response = {
+            'email': None,
+            'is_active': False,
+            'name': 'One',
+            'surname': 'student',
+            'teacher': [1],
+        }
 
+        self.assertEqual(response.json(), base_response)
+
+    # def test_student_update(self):
+    #     Teacher.objects.create(name_surname='Ted')
+    #     Student.objects.create(name='One',
+    #                            surname='Student',
+    #                            )
+    #     students = Student.objects.all()
+    #     response = self.client.put(reverse('students_api-list', kwargs={'pk': students[0].id}),
+    #                                {
+    #                                     'name': 'Two',
+    #                                     'surname': 'student',
+    #                                     'teacher': [1],
+    #                                })
+    #     base_response = {
+    #         'email': None,
+    #         'is_active': False,
+    #         'name': 'One',
+    #         'surname': 'student',
+    #         'teacher': [1],
+    #     }
+    #
+    #     self.assertEqual(response.json(), base_response)
 
 
 
